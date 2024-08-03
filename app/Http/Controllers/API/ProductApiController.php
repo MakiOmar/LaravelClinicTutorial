e7bfca52d89e31c918734794257fd917c822e756
makiomar <mo7amed.maki@gmail.com>
2024-08-03 16:28:49 +0300

    Finish product api controller

--- app/Http/Controllers/API/ProductApiController.php
+++ app/Http/Controllers/API/ProductApiController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Http\Controllers\API\RespController;
+use Illuminate\Support\Facades\Validator;
+use App\Models\ProductTage;
 
 class ProductApiController extends Controller
 {
@@ -20,6 +22,115 @@ class ProductApiController extends Controller
         $user     = Auth()->user();
         $products = $user->products;
 
-        return $this->successResponse($user, 'Success', 200);
+        return $this->successResponse($products, 'Success', 200);
+    }
+    protected function validation($request)
+    {
+        $validator = Validator::make(
+            $request->all(),
+            array(
+                'title'       => 'required|string|max:255',
+                'description' => 'required|string',
+                'price'       => 'required|numeric|min:0',
+                'rate'        => 'required|integer|min:1|max:5',
+                'tag_id'      => 'sometimes|nullable|string',
+            )
+        );
+        return $validator;
+    }
+
+        /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validator = $this->validation($request);
+        // Perform the validation
+        if ($validator->fails()) {
+            // Validation fails
+            return $this->errorResponse('In correct data!', 422, $validator->errors());
+        }
+        $product   = Product::create(
+            array(
+                'user_id'     => Auth()->id(),
+                'title'       => $request['title'],
+                'description' => $request['description'],
+                'price'       => $request['price'],
+                'rate'        => $request['rate'],
+            )
+        );
+        if (! empty($request['tag_id'])) {
+            $TagsIds = explode(',', $request['tag_id']);
+            foreach ($TagsIds as $tag_id) {
+                ProductTage::create(
+                    array(
+                        'product_id' => $product->id,
+                        'tag_id'     => $tag_id,
+                    )
+                );
+            }
+        }
+        return $this->successResponse($product, 'Success', 200);
+    }
+    public function hasTag($product_id, $tag_id)
+    {
+        return ProductTage::where('product_id', $product_id)->where('tag_id', $tag_id)->first();
+    }
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Product $product)
+    {
+        $validator = $this->validation($request);
+        // Perform the validation
+        if ($validator->fails()) {
+            // Validation fails
+            return $this->errorResponse('In correct data!', 422, $validator->errors());
+        }
+        if ($product->user_id != Auth()->id()) {
+            return $this->errorResponse('Unauthorized', 403, 'Forbidden');
+        }
+        $product->title       = $request['title'];
+        $product->description = $request['description'];
+        $product->price       = $request['price'];
+        $product->rate        = $request['rate'];
+
+        // Find the existing ProductTag record.
+        $productTags = $product->tags->pluck('id')->toArray();
+        // Update the existing record or create a new one.
+        if (! empty($request['tag_id'])) {
+            $TagsIds = explode(',', $request['tag_id']);
+            if ($productTags) {
+                foreach ($productTags as $tag_id) {
+                    if (! in_array($tag_id, $TagsIds)) {
+                        $product->tags()->detach($tag_id);
+                    }
+                }
+            }
+
+            foreach ($TagsIds as $tag_id) {
+                if (! $this->hasTag($product->id, $tag_id)) {
+                    ProductTage::create(
+                        array(
+                            'product_id' => $product->id,
+                            'tag_id'     => $tag_id,
+                        )
+                    );
+                }
+            }
+        }
+        $product->save();
+        return $this->successResponse('Product has been updated', 'Success', 200);
+    }
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product)
+    {
+        if ($product->delete()) {
+            return $this->successResponse('Product has deleted successfully.', 'Success', 200);
+        } else {
+            return $this->errorResponse('Failed to delete the product.', 422, 'Error');
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\API\DemoController;
 use App\Http\Controllers\API\LoginController as ApiLoginController;
 use App\Http\Controllers\API\LogoutController as ApiLogoutController;
 use App\Http\Controllers\API\RegistrationController as RegController;
@@ -22,49 +21,47 @@ use App\Http\Controllers\API\ProductApiController;
 |
 */
 
-Route::middleware(array( 'auth.gate' ))
-    ->group(
-        function () {
-            Route::get(
-                '/user',
-                function (Request $request) {
-                    return $request->user();
-                }
-            );
-            Route::post('/logout', ApiLogoutController::class);
-            Route::group(
-                array( 'prefix' => 'user' ),
+Route::middleware(['auth:sanctum'])
+   ->group(function () {
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        });
+        Route::middleware(array( 'auth.gate' ))
+            ->group(
                 function () {
-                    Route::get('/all', array( UserApiController::class, 'index' ));
-                    Route::get('/show/{user}', array( UserApiController::class, 'show' ));
-                    Route::post('/create', array( UserApiController::class, 'store' ));
-                    Route::put('/update/{user}', array( UserApiController::class, 'update' ));
-                    Route::delete('/delete/{user}', array( UserApiController::class, 'destroy' ));
-                }
-            );
+                    Route::post('/logout', ApiLogoutController::class);
+                    Route::group(
+                        array( 'prefix' => 'user' ),
+                        function () {
+                            Route::get('/all', array( UserApiController::class, 'index' ));
+                            Route::get('/show/{user}', array( UserApiController::class, 'show' ));
+                            Route::post('/create', array( UserApiController::class, 'store' ));
+                            Route::put('/update/{user}', array( UserApiController::class, 'update' ));
+                            Route::delete('/delete/{user}', array( UserApiController::class, 'destroy' ));
+                        }
+                    );
 
-            Route::group(
-                array( 'prefix' => 'product' ),
-                function () {
-                    Route::get('/all', array( ProductApiController::class, 'index' ));
-                    /*Route::get('/show/{user}', array( UserApiController::class, 'show' ));
-                    Route::post('/create', array( UserApiController::class, 'store' ));
-                    Route::put('/update/{user}', array( UserApiController::class, 'update' ));
-                    Route::delete('/delete/{user}', array( UserApiController::class, 'destroy' ));*/
+                    Route::group(
+                        array( 'prefix' => 'product' ),
+                        function () {
+                            Route::get('/all', array( ProductApiController::class, 'index' ));
+                            //Route::get('/show/{product}', array( ProductApiController::class, 'show' ));
+                            Route::post('/create', array( ProductApiController::class, 'store' ));
+                            Route::put('/update/{product}', array( ProductApiController::class, 'update' ));
+                            Route::delete('/delete/{product}', array( ProductApiController::class, 'destroy' ));
+                        }
+                    );
+                    Route::group(
+                        array( 'prefix' => 'tag' ),
+                        function () {
+                            Route::get('/all', array( TagApiController::class, 'index' ));
+                            Route::post('/create', array( TagApiController::class, 'store' ));
+                            Route::delete('/delete/{tag}', array( TagApiController::class, 'destroy' ));
+                        }
+                    );
                 }
             );
-            Route::group(
-                array( 'prefix' => 'tag' ),
-                function () {
-                    Route::get('/all', array( TagApiController::class, 'index' ));
-                    Route::post('/create', array( TagApiController::class, 'store' ));
-                    Route::delete('/delete/{tag}', array( TagApiController::class, 'destroy' ));
-                }
-            );
-        }
-    );
+   });
 
 Route::post('/login', ApiLoginController::class);
 Route::post('/register', RegController::class);
-
-Route::get('demo', array( DemoController::class, 'exampleMethod' ));
